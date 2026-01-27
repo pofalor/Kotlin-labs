b741a3cb9a4b0eaa037bf84dd57cb8198011cd45
Даниил <gromovdk@stud.kai.ru>
2026-01-27 20:19:19 +0300

    Move project files into 'Unit 1' and 'Unit 2' directories
    All files from several projects (BusinessCard, ComposeArticle, ComposeQuadrant, ComposeSuccess, HappyBirthday, example, greeting_card) were moved into new 'Unit 1' subdirectories, and new files were added under 'Unit 2/trafic light'. This change organizes the repository by unit, improving project structure and clarity.

---
+++ Unit 2/trafic light/Example kotlin (1)/src/Main.kt
@@ -0,0 +1,13 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+fun main() {
+    val trafficLightColor = "Amber"
+
+    when (trafficLightColor) {
+        "Red" -> println("Stop")
+        "Yellow", "Amber" -> println("Slow")
+        "Green" -> println("Go")
+        else -> println("Invalid traffic-light color")
+    }
+}
