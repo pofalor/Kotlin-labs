710fae450365c105c0d4be3eeb5a70439bdfd87d
Даниил <gromovdk@stud.kai.ru>
2026-01-31 16:47:45 +0300

    Добавление проекта при прохождении теста
    Add Unit 2/test_fundamentals/.gitignore with common IDE (IntelliJ, Eclipse, NetBeans, VS Code) and OS ignores. Create src/Main.kt with a simple Kotlin main that demonstrates equality, range membership, type checking, and modulus operations as a starter for fundamentals exercises.

---
+++ Unit 2/test_fundamentals/src/Main.kt
@@ -0,0 +1,11 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+    val x = 5
+    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+    // to see how IntelliJ IDEA suggests fixing it.
+    println(x == 5)
+    println(x in 1..5)
+    println(x is Int)
+    println(x % 5)
+}
