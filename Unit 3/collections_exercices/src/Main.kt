5e08007b5867e0f6b7ad688f5a7387ef8c0075de
Даниил <daniil.grra@gmail.com>
2026-03-10 21:08:48 +0300

    Правки в проекте с объектами
    Правки, чтобы первый ивент выводился через расширение базового класса (добавление нового свойства), также были вынесены ивенты в другую область видимости (вынесены из main функции)

--- Unit 3/collections_exercices/src/Main.kt
+++ Unit 3/collections_exercices/src/Main.kt
@@ -15,14 +15,22 @@ public enum class Daypart {
     EVENING
 }
 
+val Event.durationOfEvent: String
+    get() = if (events[0].durationInMinutes < 60) {
+        "short"
+    } else {
+        "long"
+    }
+
+val event1 = Event(title = "Wake up", description = "Time to get up", daypart = Daypart.MORNING, durationInMinutes = 0)
+val event2 = Event(title = "Eat breakfast", daypart = Daypart.MORNING, durationInMinutes = 15)
+val event3 = Event(title = "Learn about Kotlin", daypart = Daypart.AFTERNOON, durationInMinutes = 30)
+val event4 = Event(title = "Practice Compose", daypart = Daypart.AFTERNOON, durationInMinutes = 60)
+val event5 = Event(title = "Watch latest DevBytes video", daypart = Daypart.AFTERNOON, durationInMinutes = 10)
+val event6 = Event(title = "Check out latest Android Jetpack library", daypart = Daypart.EVENING, durationInMinutes = 45)
+val events = mutableListOf<Event>(event1, event2, event3, event4, event5, event6)
+
 fun main() {
-    val event1 = Event(title = "Wake up", description = "Time to get up", daypart = Daypart.MORNING, durationInMinutes = 0)
-    val event2 = Event(title = "Eat breakfast", daypart = Daypart.MORNING, durationInMinutes = 15)
-    val event3 = Event(title = "Learn about Kotlin", daypart = Daypart.AFTERNOON, durationInMinutes = 30)
-    val event4 = Event(title = "Practice Compose", daypart = Daypart.AFTERNOON, durationInMinutes = 60)
-    val event5 = Event(title = "Watch latest DevBytes video", daypart = Daypart.AFTERNOON, durationInMinutes = 10)
-    val event6 = Event(title = "Check out latest Android Jetpack library", daypart = Daypart.EVENING, durationInMinutes = 45)
-    val events = mutableListOf<Event>(event1, event2, event3, event4, event5, event6)
     events.forEach {
         println(it)
     }
@@ -47,4 +55,5 @@ fun main() {
         "long"
     }
     println("Duration of first event of the day: $durationOfEvent")
+    println("Duration of first event of the day: ${events.first().durationOfEvent}")
 }
