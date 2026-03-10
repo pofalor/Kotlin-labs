16c17505389f93680f7e5a6904bb474c8bdb686d
Даниил <daniil.grra@gmail.com>
2026-03-10 20:48:21 +0300

    Рефакторинг в 3 юните в проекте объектов
    Вместо беспорядочных ивентов теперь используется MutableList

--- Unit 3/collections_exercices/src/Main.kt
+++ Unit 3/collections_exercices/src/Main.kt
@@ -21,12 +21,8 @@ fun main() {
     val event4 = Event(title = "Practice Compose", daypart = Daypart.AFTERNOON, durationInMinutes = 60)
     val event5 = Event(title = "Watch latest DevBytes video", daypart = Daypart.AFTERNOON, durationInMinutes = 10)
     val event6 = Event(title = "Check out latest Android Jetpack library", daypart = Daypart.EVENING, durationInMinutes = 45)
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-    println(event1)
-    println(event2)
-    println(event3)
-    println(event4)
-    println(event5)
-    println(event6)
+    val events = mutableListOf<Event>(event1, event2, event3, event4, event5, event6)
+    events.forEach {
+        println(it)
+    }
 }
